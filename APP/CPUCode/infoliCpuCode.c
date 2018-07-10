b7ff2246d6c2d11d62930cf587d4004b19dd0a49
Georgios Sm <turin231@protonmail.com>
2018-07-10 16:06:57 +0200

    Features update

--- APP/CPUCode/infoliCpuCode.c
+++ APP/CPUCode/infoliCpuCode.c
@@ -6,16 +6,17 @@
 #include "MaxSLiCInterface.h"
 
 
-#define SIMTIME 6000// in ms, for when no input file is provided
-
+#define SIMTIME 1// in ms, for when no input file is provided
 //Change the Time Mux Factor to change the number of simulated cells in the network
 ////////------Keep Netwotk size in multiples of 96 cells for channel alignment----////////
-#define TIME_MUX_FACTOR 384
+#define TIME_MUX_FACTOR 3840
 
 ///----DO NOT TOUCH defines after this point--///
 #define HW_CELLS 1
-//Number of parameters for states
+//Number of parameters for states + filler
 #define INI_PARAMETERS 24
+//Number of parameters for Neuron Characteristics  + filler
+#define NEURON_PARAMETERS 24
 //Maximum Number of cells and time-multiplexing
 #define MAX_N_SIZE 7680 //Make sure this has the same value in the kernel code (NetMaxSize)
 //#define MAX_TIME_MUX 10
@@ -36,7 +37,7 @@ static timestamp getTimestamp(){
 }
 
 
-int main(void)
+int main(int argc,char *argv[])
 {
 
     char *outFileName = "InferiorOlive_Output.txt";
@@ -49,26 +50,49 @@ int main(void)
     float V,f, IC;
     float Facc = 0.0;
     float Vacc = 0.0;
+    long NetSize = 0;
+    long TimeMuxFactor= 0;
 
 
+
+	if (argc >1){
+		NetSize = atoi(argv[1]);
+		TimeMuxFactor = NetSize/HW_CELLS ;
+		if (atoi(argv[1])%96!=0){
+			printf("Network size must be a multiple of 96 cells");
+			return 0;
+		}
+	}
+	else{
+		NetSize = IO_NETWORK_SIZE;
+		TimeMuxFactor = TIME_MUX_FACTOR;
+	}
 
     printf("Begin execution \n") ;
-	const int IniVectorSize = INI_PARAMETERS* IO_NETWORK_SIZE;
+	const int IniVectorSize = INI_PARAMETERS* NetSize;
+
 
 
 	int sizeBytesFloat = sizeof(float);
 	float *IniVector = malloc(sizeBytesFloat * IniVectorSize);
-	float *IniICVector = malloc(sizeBytesFloat * IO_NETWORK_SIZE);
+	float *IniICVector = malloc(sizeBytesFloat * NetSize);
+	float *NeuronCharCVector = malloc(sizeBytesFloat * NEURON_PARAMETERS*NetSize);
+	float *ConnectivityVector = malloc(sizeBytesFloat * NetSize*NetSize);
 
 
 	long ticks = 0;
-	long TimeMuxFactor = TIME_MUX_FACTOR;
+
 	long MaxNSize = MAX_N_SIZE;
-	long NetSize = IO_NETWORK_SIZE;
+
+
 	//long MaxTimeMux = MAX_TIME_MUX;
 
+	max_file_t *maxfile = infoli_init();
+	long BurstLengthInBytes = max_get_burst_size(maxfile, "cmd_tolmem");
+	printf("Burst size: %ld \n",  BurstLengthInBytes);
 
-	// Cell Parameters Initialization
+
+	// Cell State Initialization
 	float initOneCell[] = {
 	/*0*/		-60, //V_dend
 	/*1*/		0.0112788,// High-threshold calcium_r
@@ -101,19 +125,72 @@ int main(void)
 
 			 };
 
+	// Neuron Characteristics Initialization
+	float CharactInit[] = {
+				// Capacitance
+	/*0*/		1.0, //CM
+				// Somatic conductances (mS/cm2)
+	/*1*/		150,// G_NA_S
+	/*2*/		9.0,// G_KDR_S
+	/*3*/		5.0,// G_K_S
+	/*4*/		0.016,// G_LS
+				// Dendritic conductances (mS/cm2)
+	/*5*/		35.0,// G_K_CA
+	/*6*/		4.5, //G_CAH
+	/*7*/		0.016, // G_LD
+	/*8*/		0.125,// G_H
+				// Axon hillock conductances (mS/cm2)
+	/*9*/		240.0, //G_NA_A
+	/*10*/		20.0,// G_K_A
+	/*11*/		0.016, // G_LA
+				// Cell morphology
+	/*12*/    	0.25,// P1
+	/*13*/		0.15,// P2
+	/*14*/		0.13,// G_INT
+				// Reversal potentials
+	/*15*/		55.0, // V_NA
+	/*16*/		-75.0,// V_K
+	/*17*/		120.0,// V_CA
+	/*18*/		-43.0,// V_H
+	/*19*/		10,//V_L
+				//filler for channel alignment
+				0,
+				0,
+				0,
+				0
+
+			 };
+
 //Create initial state input for all cells
 	printf("Creating State Initialization Vector\n");
 	for(long i = 0; i< IniVectorSize ; i=i+INI_PARAMETERS){
 		for(long j = 0; j<INI_PARAMETERS; j++){
 			IniVector[i+j] = initOneCell[j];
+
+		}
+	}
+
+//Define initial cell characteristics/conductances
+	printf("Creating filler for neuron characteristics Vector\n");
+	for(long i = 0; i< NetSize ; i++){
+		//CharactInit[0]++; // This is for debugging
+		for(long j = 0; j<NEURON_PARAMETERS; j++){
+
+			NeuronCharCVector[(i*NEURON_PARAMETERS)+j] = CharactInit[j];
 			//printf(" Ini Vector : %ld \n", i+j);
 		}
 	}
 
+//Create Connectivity Vector
+	printf("Creating Connectivity Vector\n");
+	for(long k = 0; k<NetSize*NetSize; k++){
+		ConnectivityVector[k] = 0.004;
+	}
+
 //Create Initial ICs (Gap Junction Influences)
 	printf("Creating Initialization Gap Junction Vector\n");
-	for(long k = 0; k< IO_NETWORK_SIZE ; k++){
-		for(long l = 0; l< IO_NETWORK_SIZE ; l++){
+	for(long k = 0; k< NetSize ; k++){
+		for(long l = 0; l< NetSize ; l++){
 			V = IniVector[k*24] - IniVector[l*24];
 			f = V * exp(-0.01 * V * V);
 			Facc = f + Facc;
@@ -137,13 +214,14 @@ int main(void)
 
     simTime = SIMTIME; // in miliseconds
     simSteps = ceil(simTime/DELTA);
-    ticks = (simSteps * IO_NETWORK_SIZE * IO_NETWORK_SIZE);
+
+    ticks = (simSteps * NetSize * NetSize);
 //Malloc evoked input memory
-    float *iApp = malloc(sizeBytesFloat * simSteps * IO_NETWORK_SIZE);
+    float *iApp = malloc(sizeBytesFloat * simSteps * NetSize);
 //Initialise iApp trace
     printf("Creating Input Trace\n");
-    for (long i = 0 ; i < simSteps * IO_NETWORK_SIZE ; i++){
-    	if (i > ((20000-1) * IO_NETWORK_SIZE) + IO_NETWORK_SIZE-1 && i < (20500-1) * IO_NETWORK_SIZE){
+    for (long i = 0 ; i < simSteps * NetSize ; i++){
+    	if (i > ((20000-1) * NetSize) + NetSize-1 && i < (20500-1) * NetSize){
     		iApp[i]= 6.0;
 
     	}
@@ -152,37 +230,42 @@ int main(void)
     	}
     }
 //Malloc output memory
-    long sizeBytesFloatOut = simSteps*IO_NETWORK_SIZE* sizeBytesFloat;
+    long sizeBytesFloatOut = simSteps*NetSize* sizeBytesFloat;
     printf("Mallocing Output Space\n");
     float *Out = malloc(sizeBytesFloatOut);
 //Write ini state and input memory RAM
 	printf("Writing Initialization State to LMem.\n");
-	infoli_writeLMem(INI_PARAMETERS * IO_NETWORK_SIZE, 0, IniVector);
+	infoli_writeLMem(INI_PARAMETERS * NetSize, 0, IniVector);
 	printf("Writing Input Trace State to LMem.\n");
-	infoli_writeLMem( simSteps*IO_NETWORK_SIZE, INI_PARAMETERS * (MaxNSize), iApp);
+	infoli_writeLMem( simSteps*NetSize, INI_PARAMETERS * (MaxNSize), iApp);
 	printf("Writing IC Initialization Trace State to LMem.\n");
-	infoli_writeLMem( IO_NETWORK_SIZE, (INI_PARAMETERS * (MaxNSize))+ (simSteps*(MaxNSize)+(simSteps * (MaxNSize))),IniICVector);
+	infoli_writeLMem( NetSize, (INI_PARAMETERS * (MaxNSize))+ (simSteps*(MaxNSize)),IniICVector);
+	printf("Writing Neuron Characteristics Trace to LMem.\n");
+	infoli_writeLMem( NEURON_PARAMETERS*NetSize, (INI_PARAMETERS * (MaxNSize))+ (simSteps*(MaxNSize)) + MaxNSize,NeuronCharCVector);
+	printf("Writing Connectivity Vector Trace State to LMem.\n");
+	infoli_writeLMem( NetSize*NetSize, (INI_PARAMETERS * (MaxNSize))+ simSteps*(MaxNSize)+ MaxNSize + (NEURON_PARAMETERS*MaxNSize) ,ConnectivityVector);
 
-
+	printf("conductance Vector Address: %ld \n",  (INI_PARAMETERS * (MaxNSize))+ simSteps*(MaxNSize)+ MaxNSize );
 //execute kernel
 	printf("Running on DFE. Net Size = %ld and SimSteps = %ld\n", NetSize, simSteps);
 	t0 = getTimestamp();
-	infoli(MaxNSize, ticks, NetSize, simSteps, TimeMuxFactor);
+	infoli(BurstLengthInBytes, MaxNSize, ticks, NetSize, simSteps, TimeMuxFactor);
 	t1 = getTimestamp();
 
 	printf("DFE Runtime = %9.7f seconds\n", (t1-t0)/1000000.0 );
 
 	printf("Reading memory\n");
 	fflush(stdout);
-	infoli_readLMem( simSteps * IO_NETWORK_SIZE ,(INI_PARAMETERS * (MaxNSize))+ (simSteps*(MaxNSize)), Out);
+	infoli_readLMem( simSteps * NetSize, (INI_PARAMETERS * (MaxNSize))+ simSteps*(MaxNSize)+ MaxNSize + (NEURON_PARAMETERS*MaxNSize)+ MaxNSize*MaxNSize, Out);
+
 
 //Write output to file
 	printf("Writing Output File\n");
 	for (long i=0; i<simSteps ; i++){
-		sprintf(temp, "%ld %.2f ", i+1, iApp[i* IO_NETWORK_SIZE]);
+		sprintf(temp, "%ld %.2f ", i+1, iApp[i* NetSize]);
     	fputs(temp, pOutFile);
     	int cell = -1;
-		for (long j=i*IO_NETWORK_SIZE; j<(i*IO_NETWORK_SIZE)+IO_NETWORK_SIZE; j++){
+		for (long j=i*NetSize; j<(i*NetSize)+NetSize; j++){
 			cell++;
 			sprintf(temp, " %d : %.8f ",cell, Out[j]);
 			fputs(temp, pOutFile);
--- README.md
+++ README.md
@@ -9,7 +9,7 @@ the Neuroscience department of the Erasmus Medical Center.
 
 ## Description
 
-The BrainFrame demo demonstrates the simulation of an Inferior Olive network model. A crucial component of the Olivorcerebellar circuit imperative for the control and synchronization of motor functions. The model is based on a highly complex, bio-physically meaningful and computationally demanding neuron model (Hodgkin-Huxley) and also supports Gap Junction connectivity between neurons. It supports maximally a netwrok of 7680 neurons and has a real-time capability (simulating the network with real-time speed) of about 400 neurons. The network is all-to-all interconnected (representing worst cased scenario computationally)
+The BrainFrame demo demonstrates the simulation of an Inferior Olive network model. A crucial component of the Olivorcerebellar circuit imperative for the control and synchronization of motor functions. The model is based on a highly complex, bio-physically meaningful and computationally demanding neuron model (Hodgkin-Huxley) and also supports Gap Junction connectivity between neurons. It supports maximally a netwrok of 7680 neurons and has a real-time capability (simulating the network with real-time speed) of about 400 neurons. The application supports programmable connectivity between neurons as well as fully programable neuron conductances. It has been tested and validated with Maxeler tools 2016.1.1 and DFE MAX4 hardware. 
 
 ## Content
 
